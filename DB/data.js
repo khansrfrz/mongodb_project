5f95d8d10ae3f544782ebec46b15277ea97db64e
khansrfrz <khansarfaraz205@gmail.com>
2023-02-14 16:57:13 +0530

    v1.02

--- DB/data.js
+++ DB/data.js
@@ -1,9 +1,9 @@
 const UserData = {
-    Name:"Arbaaz",
+    Name:"Sarfaraz",
     Age:24,
     Address:"Bhilai",
     flightNo:7856532600,
-    Travel_btw_cities:"Raipur to Chennai",
+    Travel_btw_cities:"Raipur to banglore",
     flight_Timings:"10:00 to 12:00",
     plane_Category:"cargo"
 }
